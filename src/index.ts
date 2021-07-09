07990ff7243c58a3367a5a74cfde85f5987d8721
nicholas_roether <nicholas.roether@t-online.de>
2021-07-09 03:05:30 +0200

    create api methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-export {};
+import { IncomingMessage } from "http";
+import { URL } from "url";
+import Request, { RequestAbortedError, RequestOptions, RequestTimeoutError } from "./request";
+
+export type RequestOptionsWithoutMethod = Omit<RequestOptions, "method">;
+
+
+function request(url: string | URL, options: RequestOptions, cb?: ((res: IncomingMessage) => void) | undefined) {
+	return new Request(url, options, cb);
+}
+
+function get(url: string | URL, options: RequestOptionsWithoutMethod, cb?: ((res: IncomingMessage) => void) | undefined) {
+	return new Request(url, {...options, method: "GET"}, cb);
+}
+function post(url: string | URL, options: RequestOptionsWithoutMethod, cb?: ((res: IncomingMessage) => void) | undefined) {
+	return new Request(url, {...options, method: "POST"}, cb);
+}
+
+function head(url: string | URL, options: RequestOptionsWithoutMethod, cb?: ((res: IncomingMessage) => void) | undefined) {
+	return new Request(url, {...options, method: "HEAD"}, cb);
+}
+
+export type { RequestOptions }
+
+export {
+	RequestAbortedError,
+	RequestTimeoutError,
+	request,
+	get,
+	post,
+	head
+}
